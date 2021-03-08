918a9da58679ea0ef73386f1e033e629d609287d
Tagne Nguifo Eric Hermann <ericnguifo@gmail.com>
2021-03-08 23:35:10 +0100

    Whitecow project

--- WhiteCow/settings.py
+++ WhiteCow/settings.py
@@ -139,3 +139,10 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Africa/Lagos'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ertagne@gmail.com'
+EMAIL_HOST_PASSWORD = 'eric2021?'
