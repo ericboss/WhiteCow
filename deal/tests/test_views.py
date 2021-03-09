be0ba56d4c2169fd300055e1e8387fd971226302
Tagne Nguifo Eric Hermann <ericnguifo@gmail.com>
2021-03-09 09:47:51 +0100

    updating test views

---
+++ deal/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from deal.models import Deals, ComputeDeals, Adress,AssetTypes
+import json
+
+class TestViews(TestCase):
+    client = Client()
+    response = client.get(reverse('index'))
+
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, '')
+
+
