bbad05edc35e354f524d8ad1e2b95d476627442b
Tagne Nguifo Eric Hermann <ericnguifo@gmail.com>
2021-03-09 13:34:49 +0100

    test form

--- deal/tests/test_forms.py
+++ deal/tests/test_forms.py
@@ -1,10 +1,11 @@
 from deal.forms import DealAddressForm, DealAssetTypeForm, DealComputeDealForm, DealsForm
-from django.test import TestCase
+from django.test import TestCase, Client
 from deal.models import Deals, ComputeDeals, Adress,AssetTypes
 from django.contrib.auth.models import User
 
 class TestForms(TestCase):
     def setUp(self):
+        self.client = Client()
         self.address1 = Adress.objects.create(city="New York City", state_code="NY")
         self.asset1 = AssetTypes.objects.create()
         self.compute1 = ComputeDeals.objects.create(period='1 month', compare='below', percentage_compare_average_price=0)
@@ -22,6 +23,7 @@ class TestForms(TestCase):
 
 
     def test_adress_form_valid_data(self):
+        login = self.client.login(username='testuser', password='12345')
         address = Adress.objects.get(pk = self.pk)
         form_adress = DealAddressForm(instance=address)
         self.assertTrue(form_adress.is_valid()) 
