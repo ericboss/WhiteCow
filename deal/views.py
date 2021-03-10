185eab9f6663ee428bdee3b74164b1a58793a9f2
Tagne Nguifo Eric Hermann <ericnguifo@gmail.com>
2021-03-10 08:26:07 +0100

    update views

--- deal/views.py
+++ deal/views.py
@@ -6,10 +6,18 @@ from .forms import DealAddressForm, DealAssetTypeForm, DealComputeDealForm, Deal
 from django.contrib.auth.decorators import login_required
 
 def index(request):
+    """
+    This view is to display the landing page
+    """
     return render(request, 'deal/index.html')
 
 @login_required
 def create_deal(request):
+    """
+    This view will present the forms the user will fill inorder to get create a deal request.
+    If the forms(Adress, Asset properties, Compute Deals and Deals) are valid,the forms will be saved in their corresponding
+    model classes.The class will also be added to the user so as to get deal per user 
+    """
     if request.method == 'POST':
         form_deal = DealsForm(request.POST)
         form_adress = DealAddressForm(request.POST)
@@ -35,6 +43,12 @@ def create_deal(request):
     return render(request, 'deal/deal-form.html', {'deal': form_deal,'adress':form_adress, 'compute':form_compute, 'asset':form_asset})
 @login_required
 def deal_diaply_on_search(request):
+    """
+    This view is for displaying the result of creating a deal.It presents the user with form whose intance values corresponds to what the user 
+    entered as initial data. The view will also display the result of the deal created.The form can be updated 
+    If the forms(Adress, Asset properties, Compute Deals and Deals) are valid,the forms will be saved in their corresponding
+    model classes.The class will also be added to the user so as to get deal per user 
+    """
 
     deal = Deals.objects.latest('id')
     address = Adress.objects.latest('id')
@@ -54,7 +68,14 @@ def deal_diaply_on_search(request):
             address = Adress.objects.latest('id')
             asset = AssetTypes.objects.latest('id')
             compute = ComputeDeals.objects.latest('id')
-            Deals.objects.latest('id').update(name=name, property_status=property_status, adress=address,assets=asset,computeDeal=compute)
+            deal_update = Deals.objects.latest('id')
+            #update(name=name, property_status=property_status, adress=address,assets=asset,computeDeal=compute)
+            deal_update.name = name
+            deal_update.property_status = property_status
+            deal_update.adress = address
+            deal_update.assets= asset
+            deal_update.computeDeal = compute
+            deal_update.save()
             deal = Deals.objects.latest('id')
 
     else:
@@ -71,12 +92,19 @@ def deal_diaply_on_search(request):
 
 @login_required
 def manage_subscriptions(request):
+    """
+    Thie view is to display the subcriptions(saved deals) a user has made
+    """
     
     #deal = Deals.objects.all()
     return render(request, 'deal/subscriptions.html')
 
 @login_required      
 def edit(request, pk):
+    """
+    Edit view is to saved deal. The user is presented with a form with instance values corresponding to the data 
+    the user saved. The user can edit the form and save. The page will be redirected to the page to view saved subscriptions upon save
+    """
     deal = Deals.objects.get(pk = pk)
     if request.method == 'POST':
         deal_form = DealsForm(request.POST, instance = deal)
@@ -89,6 +117,10 @@ def edit(request, pk):
 
 @login_required   
 def delete(request, pk):
+    """
+    delete view is to saved deal. The user is presented to delete page upon request. 
+    The deal is deleted upon confirmtion. The page will be redirected to the page to view saved subscriptions upon delete
+    """
 
     deal = Deals.objects.get(pk = pk) 
     if request.method =='POST':
@@ -98,6 +130,12 @@ def delete(request, pk):
 
 @login_required
 def deal_diaplay_specific_search_id(request, pk):
+    """
+    This view is to view a saved deal.It presents the user with form whose intance values corresponds to what the user 
+    entered as initial data. The view will also display the result of the deal created.The form can be updated 
+    If the forms(Adress, Asset properties, Compute Deals and Deals) are valid,the forms will be saved in their corresponding
+    model classes.The class will also be added to the user so as to get deal per user 
+    """
 
     deal = Deals.objects.get(pk = pk) 
     address = Adress.objects.get(pk = pk) 
